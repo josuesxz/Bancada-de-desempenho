0872da05edc2035a7dd474fad0ef8ff0d85f7bd0
sxjosue <133665545+sxjosue@users.noreply.github.com>
2024-04-28 17:38:28 -0400

    Desenvolvimento da PCB
    tá difícil

--- BANCADA_DESEMPENHO_04/BANCADA_DESEMPENHO_04.ino
+++ BANCADA_DESEMPENHO_04/BANCADA_DESEMPENHO_04.ino
@@ -9,7 +9,7 @@ DHT dht(DHTPIN, DHTTYPE);
 double temperatura;
 
 #define DOUT  A1                      // HX711 DATA OUT = pino A0 do Arduino 
-#define CLK   A0                      // HX711 SCK IN = pino A1 do Arduino e dht no A2
+#define CLK   A0                      // HX711 SCK IN = pino A1 do Arduino 
 
 HX711 balanca;                        // define instancia balança HX711
 float calibration_factor = 12169.9293;     // Mudar em função do sensor
@@ -28,18 +28,18 @@ void setup(){
   Wire.begin();
   Serial.begin(9600);
   begin_temperatura();
-  //begin_balanca();
-  //begin_vcas();
-  //begin_tac();
+  begin_balanca();
+  begin_vcas();
+  begin_tac();
   difPressure = 0.0;
   Serial.println("Valores em fileira de: Balança, Temperatura, Pressão e Velocidade");
 }
 
 void loop(){
   loop_temperatura();
-  //loop_balanca(); 
-  //loop_vcas();
-  //loop_tac();
+  loop_balanca(); 
+  loop_vcas();
+  loop_tac();
   Serial.print("\n");    
   delay(500);
 }
