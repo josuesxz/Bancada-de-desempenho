05ba17b1cb736538fd4ac5d29c5de41f4b207f93
Josué Souza <99757018+josuesxz@users.noreply.github.com>
2024-06-12 14:08:33 -0400

    .

--- BANCADA_DESEMPENHO_06/Balanca.ino
+++ BANCADA_DESEMPENHO_06/Balanca.ino
@@ -1,12 +1,12 @@
 void begin_balanca(){
   balanca.begin(DOUT, CLK);                          // inicializa a balança
-  balanca.set_scale();      // ajusta fator de calibração
+  balanca.set_scale(calibration_factor);      // ajusta fator de calibração
   balanca.tare();                                    // zera a Balança
 }
 
 void loop_balanca(){
   balanca.power_up(); // LIGANDO O SENSOR
-  Serial.print((balanca.get_units(), 3)*9.8066); //lendo e convertendo para de Kgf para N
+  Serial.print((balanca.get_units(), 3)); //lendo e convertendo para de Kgf para N (1Kgf = 9.8066N)
   Serial.print(";"); 
   balanca.power_down(); // DESLIGANDO O SENSOR
 }
